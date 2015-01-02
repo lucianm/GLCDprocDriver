e6affc75f7a085a9d2eedd16e3b72ce7eb71c5f1
Lucian Muresan <Lucian.Muresan@siemens.com>
2015-01-02 21:33:06 +0100

    index.html: dynamically getting README.md contents

---
+++ javascripts/convert_README.js
@@ -0,0 +1,21 @@
+// Choose branch based on query string
+var query = window.location.search;
+if (query) {
+	var branch = query.replace("?", "");
+} else {
+	var branch = "master";
+}
+
+// Load readme content
+$.ajax({
+	url: "https://rawgit.com/lucianm/GLCDprocDriver/"+branch+"/README.md",
+	dataType: 'text',
+	success: function(data) {
+		
+		// Convert readme from markdown to html
+		var converter = new Markdown.Converter();
+
+		// Show html
+		$(".wrapper").html(converter.makeHtml(data));
+	}
+});
